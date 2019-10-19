1fad867af6e95126298e409ecb236ca8b8b93cdc
anuragjha <ajha6@dons.usfca.edu>
2019-10-18 22:05:45 -0700

    new devices

---
+++ cmd/device-simple/main.go
@@ -0,0 +1,27 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+//
+// Copyright (C) 2017-2018 Canonical Ltd
+// Copyright (C) 2018-2019 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// This package provides a simple example of a device service.
+package main
+
+import (
+	"fmt"
+	"github.com/edgexfoundry/device-simple"
+	"github.com/edgexfoundry/device-simple/driver"
+	"github.com/edgexfoundry/device-sdk-go/pkg/startup"
+	"os"
+)
+
+const (
+	serviceName string = "device-simple"
+)
+
+func main() {
+	fmt.Fprintf(os.Stdout, "HERE.......\n")
+	sd := driver.SimpleDriver{}
+	startup.Bootstrap(serviceName, device.Version, &sd)
+}
