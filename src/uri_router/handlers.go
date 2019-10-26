6751b675aac0d2882740e272ad6289bf62e09448
anuragjha <ajha6@dons.usfca.edu>
2019-10-26 07:31:23 -0700

    simple make decision

--- src/uri_router/handlers.go
+++ src/uri_router/handlers.go
@@ -6,6 +6,8 @@ import (
 	"github.com/gorilla/mux"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 //var INIT_SERVER_ADDRESS = "http://localhost:6686"
@@ -157,6 +159,60 @@ func ShowAllLatestDeviceData(w http.ResponseWriter, r *http.Request) {
 
 }
 
-//func MakeDecision(w http.ResponseWriter, r *http.Request) {
-//
-//}
+func MakeDecision(w http.ResponseWriter, r *http.Request) {
+
+	forDecisionSupply := make(map[string]string)
+	forDecisionConsume := make(map[string]string)
+
+	forDevices := [2]string{"Supply-Device-01/randomsuppliernumber", "Consume-Device01/randomconsumenumber"}
+	for _, ford := range forDevices {
+		resp, err := http.Get("http://localhost:6686/showLatestDeviceData/" + ford /*/Supply-Device-01/randomsuppliernumber"*/)
+		if err != nil {
+			fmt.Println("Could not fetch data for Supply-Device-01")
+		}
+		defer resp.Body.Close()
+
+		value, _ := ioutil.ReadAll(resp.Body)
+		values := string(value[:])
+		vals := strings.Split(values, "=")
+		if len(vals) > 1 {
+			if strings.Contains(vals[0], "Supply") {
+				forDecisionSupply[vals[0]] = vals[1]
+			} else if strings.Contains(vals[0], "Consume") {
+				forDecisionConsume[vals[0]] = vals[1]
+			}
+		}
+	}
+
+	// making decision
+	if len(forDecisionConsume) < 1 {
+		_, _ = w.Write([]byte("No consume device"))
+
+	} else if len(forDecisionSupply) < 1 {
+		_, _ = w.Write([]byte("No consume device"))
+
+	} else {
+		sb := strings.Builder{}
+		sb.WriteString("Pairing consume and supply devices:\n")
+		for ck, cv := range forDecisionConsume {
+			matched := false
+			sb.WriteString(">>>\n")
+			for sk, sv := range forDecisionSupply {
+				sval, _ := strconv.Atoi(sv)
+				cval, _ := strconv.Atoi(cv)
+				if sval >= cval { // one supply device supplying all energy needed by the consume device
+					matched = true
+					forDecisionSupply[sk] = string(sval - cval)
+					sb.WriteString(ck + "will receive " + cv + " units from " + sk)
+				}
+			}
+			if matched == false {
+				sb.WriteString("Could not match" + ck + "to any Supply device")
+			}
+		}
+
+		_, _ = w.Write([]byte(sb.String()))
+
+	} // end of else
+
+}
