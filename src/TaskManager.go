6a694016705fd58d9e6cb1393a755ac71a4955c6
Rozita <rozita.teim@gmail.com>
2019-10-27 17:21:41 -0700

    Device Json Parser

--- src/TaskManager.go
+++ src/TaskManager.go
@@ -9,13 +9,14 @@ import (
 )
 
 func main() {
-	fmt.Println("ok ok ")
 
 	router := uri_router.NewRouter()
 	if len(os.Args) > 1 {
 		log.Fatal(http.ListenAndServe(":"+os.Args[1], router))
+		fmt.Println("Task Manager is listening from port: ", os.Args[1])
 	} else {
 		log.Fatal(http.ListenAndServe(":6686", router))
+		fmt.Println("Task Manager is listening from port: 6686")
 	}
 
 }
--- src/parser/jsonParser.go
+++ src/parser/jsonParser.go
@@ -1,20 +1,142 @@
 package parser
 
+import (
+	"encoding/json"
+	"fmt"
+)
 
 
-type device struct {
-	id    string
-	device string
-	created string
-	modified string
-	origin string
-	reading [] string
+/* Device Struct
+*
+* Data structure for Device info
+*
+ */
+type Device struct {
+	Id       string 	`json:"id"`
+	Created  int64  		`json:"created"`
+	Origin   int64  	`json:"origin"`
+	Modified int64   	`json:"modified"`
+	Device   string 	`json:"device"`
+	Name     string 	`json:"name"`
+	Value    int32 		`json:"value"`
 }
 
+
+
+/* NewTransaction()
+*
+* To return new transaction data
+*
+ */
+func NewDevice(id string, created int64, origin int64, modified int64, device string, name string, value int32) Device {
+	return Device{
+		Id: id,
+		Created: created,
+		Origin: origin,
+		Modified: modified,
+		Device: device,
+		Name: name,
+		Value: value,
+	}
+}
+
+
+/* EncodeToJson()
+*
+* To Encode Device info from json format
+*
+ */
+func (device *Device) EncodeToJson() (string, error) {
+	jsonBytes, error := json.Marshal(device)
+	return string(jsonBytes), error
+}
+
+/* DecodeFromJson()
+*
+* To Decode Device info from json format
+*
+ */
+func (device *Device) DecodeFromJson(jsonString string) error {
+	return json.Unmarshal([]byte(jsonString), device)
+}
+
+
+/* GetId
+*
+* To Get device Id
+*
+ */
+func (device *Device) getId() string{
+	return device.Id
+}
+
+/* getCreated
+*
+* To Get device Created date
+*
+ */
+func (device *Device) getCreated() int64{
+	return device.Created
+}
+
+/* getOrigin
+*
+* To Get device Origin date
+*
+ */
+func (device *Device) getOrigin() int64{
+	return device.Origin
+}
+
+/* getModified
+*
+* To Get Modified date
+*
+ */
+func (device *Device) getModified() int64{
+	return device.Modified
+}
+
+/* getDevice
+*
+* To Get Device info
+*
+ */
+func (device *Device) getDevice() string{
+	return device.Device
+}
+
+/* getDeviceName
+*
+* To Get Device Name
+*
+ */
+func (device *Device) getDeviceName() string{
+	return device.Name
+}
+
+/* getDeviceValue
+*
+* To Get Device Value
+*
+ */
+func (device *Device) getDeviceValue() int32{
+	return device.Value
+}
+
+/* printDeviceInfo
+*
+* To print device information
+*
+ */
+func (device *Device) printDeviceInfo() {
+
+	fmt.Print("Device ID: " , device.getId() , "Device Created Time: " , device.getCreated() , "Device Origin: " , device.getOrigin() , "Device Modified: " , device.getModified() , "Device Device: " , device.getDevice() , "Device Device Name: " , device.getDeviceName() , "Device Value: " , device.getDeviceValue())
+}
+
+
 func main() {
-	//deviceJson := `[{"species":"pigeon","decription":"likes to perch on rocks"},{"species":"eagle","description":"bird of prey"}]`
-	//var birds []Bird
-	//json.Unmarshal([]byte(birdJson), &birds)
-	//fmt.Printf("Birds : %+v", birds)
-	////Birds : [{Species:pigeon Description:} {Species:eagle Description:bird of prey}]
+
+
+
 }
