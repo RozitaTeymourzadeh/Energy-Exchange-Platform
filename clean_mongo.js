9ca39aa283ef6ea64edafba4686f10ca8edbf337
anuragjha <ajha6@dons.usfca.edu>
2019-11-05 18:54:34 -0800

    changed supplydevice and consumedevice yaml, congiguration toml and simpleDriver.go, edited DeviceManager, suppliertx and comsumetx value change in file. TODO: add filename as variable, change make decision, but before change mechanism to handle multiple event for a device. Build a device object to store details of the device (chargeValue, Rate, switch)

---
+++ clean_mongo.js
@@ -0,0 +1,23 @@
+MongoDB shell version v4.2.1
+connecting to: mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb
+Implicit session: session { "id" : UUID("7af2a761-bcb8-48fd-a807-0dd1323514b9") }
+MongoDB server version: 4.2.1
+Server has startup warnings: 
+2019-11-05T18:49:28.192-0800 I  CONTROL  [initandlisten] 
+2019-11-05T18:49:28.192-0800 I  CONTROL  [initandlisten] ** WARNING: Access control is not enabled for the database.
+2019-11-05T18:49:28.192-0800 I  CONTROL  [initandlisten] **          Read and write access to data and configuration is unrestricted.
+2019-11-05T18:49:28.192-0800 I  CONTROL  [initandlisten] 
+---
+Enable MongoDB's free cloud-based monitoring service, which will then receive and display
+metrics about your deployment (disk utilization, CPU, operation statistics, etc).
+
+The monitoring data will be available on a MongoDB website with a unique URL accessible to you
+and anyone you share the URL with. MongoDB may use this information to make product
+improvements and to suggest MongoDB products and deployment options to you.
+
+To enable free monitoring, run the following command: db.enableFreeMonitoring()
+To permanently disable this reminder, run the following command: db.disableFreeMonitoring()
+---
+
+> [3G[J[3G^C
+bye
